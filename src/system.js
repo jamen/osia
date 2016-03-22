6ee2c34aeecc5f3e17c331782773a453b1c341a3
= <hello@imsean.me>
2016-03-21 19:38:44 -0500

    Remove sub-tasks entirely

--- src/system.js
+++ src/system.js
@@ -3,7 +3,6 @@ import Task from './task';
 import path from 'path';
 import plugin from './plugin';
 import fs from 'fs';
-import routine from 'promise-routine';
 
 class System {
   constructor(tasks = {}, name = 'osia') {
@@ -20,22 +19,14 @@ class System {
     if (args.length === 2) [route, fn] = args;
     else if (args.length === 3) [route, deps, fn] = args;
 
-    const parts = route.split(':');
-    const name = parts.splice(-1, 1);
-    const sel = this.tasks;
-
-    sel[name] = new Task(name, fn, deps);
+    this.tasks[route] = new Task(route, fn, deps);
   }
 
   run(route = 'default', opts, args) {
-    const task = this._nameToTask(route);
-    if (!(task instanceof Task)) {
-      return routine(t => this.run(`${route}:${t}`), ...Object.keys(task));
-    }
     const meta = {
       at: process.hrtime(this._startTime)[1],
     };
-    return task.start(opts, args, meta);
+    return this.tasks[Object.keys(this.tasks)[0]].start(opts, args, meta);
   }
 
   log(message) {
@@ -58,18 +49,6 @@ class System {
         (err) => (err ? reject(err) : resolve(file))
       );
     });
-  }
-
-  _nameToTask(route) {
-    const parts = route.split(':');
-    const name = parts.splice(-1, 1);
-    let sel = this.tasks;
-
-    for (const item of parts) {
-      sel = sel[item];
-    }
-
-    return sel[name];
   }
 }
 
